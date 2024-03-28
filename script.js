ecb8c9df7b36431ba1ae23e1bd54713cbecc07e5
Mariana de Veras <mariana.de.veras@eaportal.org>
2024-03-28 01:27:14 -0300

    nono commit do CSS e terceiro commit do JS

--- script.js
+++ script.js
@@ -31,6 +31,14 @@ botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagem() {
-    console.log(evento.target.value)
+function receberPorcentagemBoatao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+
+        if(botao.value === evento.target.value){
+            botao.classList.add("botao-ativo")
+        }
+    })
+
+    porcentagem = parseFloat(evento.target.value) / 100
 }
